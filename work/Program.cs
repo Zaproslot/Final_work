7197763f2761179e2949414bf113a0de670a9697
Vlad <vmereskin@gmail.com>
2023-03-11 16:12:28 +0300

    complete programm part of task

--- work/Program.cs
+++ work/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] text = {"Пытаюсь", "сделать", "итоговую", "работу", "по", "прогроммированию", ":)"};
+Console.WriteLine("{0}", string.Join(" ", text));
+
+void newText(string[] text)
+{
+    string result = string.Empty;
+    for (int i = 0; i < text.Length; i++)
+    {
+        if(text[i].Length <= 3)
+        {
+            result += $"{text[i]} ";
+        }
+    }
+
+    Console.WriteLine("{0}", string.Join(" ", result));
+}
+System.Console.WriteLine();
+newText(text);
